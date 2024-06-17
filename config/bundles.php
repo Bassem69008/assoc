c95a9a81cc2d4beb939051a6eee095cac6bed4d7
Rahali <105925297+brahali@users.noreply.github.com>
2024-06-17 11:36:38 +0200

    Feat:Pipeline-CI

--- config/bundles.php
+++ config/bundles.php
@@ -18,5 +18,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-
 ];
